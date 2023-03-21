22cab41a5a0f7a07f33cf6d16188f647efc006a5
Forrest S <thewanderinwolves@gmail.com>
2023-03-20 18:37:56 -0700

    login form styles updated

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -2,6 +2,9 @@
     let email = '';
     let password = '';
   
+    let labelClass = 'flex flex-col'
+    let inputClass = 'rounded'
+    
     function handleSubmit(event) {
       event.preventDefault();
       console.log(`Submitted: ${firstName} ${lastName} (${email}) - ${password}`);
@@ -10,13 +13,13 @@
   </script>
   
   <form on:submit={handleSubmit} class='flex flex-col border-2 w-1/4 bg-slate-300 gap-1 p-2 items-center'>
-    <label>
+    <label class={labelClass}>
       Email:
-      <input type="email" bind:value={email} required>
+      <input class={inputClass} type="email" bind:value={email} required>
     </label>
-    <label>
+    <label class={labelClass}>
       Password:
-      <input type="password" bind:value={password} required>
+      <input class={inputClass} type="password" bind:value={password} required>
     </label>
     <button type="submit">Login</button>
   </form>
