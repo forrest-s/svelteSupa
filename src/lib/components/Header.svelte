1be3acc2610cfadb3f524e822bb5e2e7ae94d32e
Forrest S <thewanderinwolves@gmail.com>
2023-03-23 19:24:23 -0700

    header styling updated.

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,5 +1,5 @@
 <nav class='flex items-center h-16 border-b-2 justify-between p-2'>
-    <h1>logo</h1>
+    <h1 class='bg-accent'>logo</h1>
     <section>
         <a href='/' class='btn btn-primary'>Home</a>
         <a href="/login" class="btn btn-outline">Login</a>
