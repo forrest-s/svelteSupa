ed22ca072ca56d6346c5e95294d83324da6b8223
Forrest S <thewanderinwolves@gmail.com>
2023-03-24 14:28:33 -0700

    nav buttons smaller.

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,8 +1,8 @@
 <nav class='flex items-center h-16 border-b-2 justify-between p-2'>
     <h1 class='bg-accent'>logo</h1>
     <section>
-        <a href='/' class='btn btn-primary'>Home</a>
-        <a href="/login" class="btn btn-outline">Login</a>
-        <a href="/register" class="btn btn-outline">Register</a>
+        <a href='/' class='btn btn-sm btn-primary'>Home</a>
+        <a href="/login" class="btn btn-sm btn-outline">Login</a>
+        <a href="/register" class="btn btn-sm btn-outline">Register</a>
     </section>
 </nav>
